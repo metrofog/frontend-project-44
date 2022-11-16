5e7ead83470c1e7a2fd005531089c1021978b78c
metrofog <metorofog1@bk.ru>
2022-11-16 14:23:59 +0300

    fix even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -4,7 +4,7 @@ const evenGame = (name) => {
 
     let count = 0;
 
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    console.log('Answer "Yes" if the number is even, otherwise answer "No".');
 
     while (count < 3) {
 
@@ -13,19 +13,21 @@ const evenGame = (name) => {
 
         let answer = readlineSync.question('Your answer: ')
 
+        if (typeof(Number(answer)) === 'number') return console.log(`This answer incorrect!(Only 'Yes' or 'No') Let's try again ${name}!`);
+
         if (number % 2 === 0) {
-             if (answer === 'yes') { 
+             if (answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'y') { 
                 console.log('Correct!'); 
                 count += 1;
             } else {
-                return console.log(`'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, ${name}!`)
+                return console.log(`'no' is wrong answer (Correct answer was 'yes'. Let's try again, ${name}!)`)
             }
         } else {
-            if (answer === 'no') { 
+            if (answer.toLowerCase() === 'no' || answer.toLowerCase() === 'n') { 
                 console.log('Correct!'); 
                 count += 1;
             } else {
-                return console.log(`'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, ${name}!`)
+                return console.log(`'yes' is wrong answer (Correct answer was 'no'. Let's try again, ${name}!)`)
             }
         }
         if (count === 3) return console.log(`Congratulations, ${name}!`)
