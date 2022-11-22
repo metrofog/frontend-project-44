16d56d5fb56e72144765bab20f50fac48435bba3
metrofog <metorofog1@bk.ru>
2022-11-22 14:13:23 +0300

    fix cli

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const sayUName = () => {
+  console.log('May I have your name?');
   const name = readlineSync.question('Write your name: ');
   console.log(`Hello, ${name}!`);
   return name;
